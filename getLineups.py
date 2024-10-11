1a4ba93e724ae449a20a8f298c6ad41edb38b3e8
Gavin Payne <g.payne2189@gmail.com>
2024-10-11 14:23:36 -0600

    H/A

--- getLineups.py
+++ getLineups.py
@@ -17,38 +17,30 @@ SheetID = os.getenv("Sheet_ID")
 sheet = client.open_by_url(f'https://docs.google.com/spreadsheets/d/{SheetID}/edit#gid=478985565')
 sh = sheet.worksheet("Lineups")
 
-# URL of the page to scrape
 url = "https://www.rotowire.com/basketball/nba-lineups.php"
 
-# Send a GET request to fetch the raw HTML content
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Find the section containing the starting lineups
 games = soup.find_all("div", class_="lineup__main")
 teamNames = soup.find_all("div", class_="lineup__abbr")
 
-# Extract the data
 lineups = []
-baseURL = "https://www.rotowire.com"  # Base URL to append to relative player links
+baseURL = "https://www.rotowire.com"
 
 for i, section in enumerate(games):
-    # Find visiting and home team abbreviations
     visitName = "..."
     homeName = "!!!"
     if i * 2 + 1 < len(teamNames):
         visitName = teamNames[i*2].text
         homeName = teamNames[i*2+1].text
 
-    # Find visiting and home team lineups
     visitingTeam = section.find("ul", class_="lineup__list is-visit")
     homeTeam = section.find("ul", class_="lineup__list is-home")
 
-    # Check if visiting and home teams were found
     if visitingTeam is None or homeTeam is None:
         continue
 
-    # Function to extract player names from a team section
     def extract_team_data(team_section):
         positions = {'PG': '', 'SG': '', 'SF': '', 'PF': '', 'C': ''}
         players = team_section.find_all("li", class_="lineup__player")
@@ -57,24 +49,19 @@ for i, section in enumerate(games):
             player_link = player.find("a")
             player_url = baseURL + player_link['href']
             player_name = "_".join([j.capitalize() for j in player_url.split("/")[-1].split("-")[0:-1]])
-            if position in positions:  # Store only if position is valid
+            if position in positions:
                 positions[position] = player_name
         return list(positions.values())
 
-    # Extract players for both teams
     visitingPlayers = extract_team_data(visitingTeam)
     homePlayers = extract_team_data(homeTeam)
 
-    # Append the lineups with team abbreviations
     lineups.append([visitName] + visitingPlayers)
     lineups.append([homeName] + homePlayers)
 
-# Convert the lineups into a DataFrame
 df = pd.DataFrame(lineups, columns=['Team', 'PG', 'SG', 'SF', 'PF', 'C'])
 
-# Update the Google Sheet with the new format
 values = [df.columns.values.tolist()] + df.values.tolist()
 sh.update("A1", values)
 
-# Output the DataFrame
 print(df)
--- homeAway.py
+++ homeAway.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import json
 import sqlite3
+import time
 
 load_dotenv()
 googleJSON = os.getenv('GOOGLE_CLOUD_SERVICE_ACCOUNT')
@@ -14,13 +15,23 @@ creds = Credentials.from_service_account_info(googleAccount, scopes=scopes)
 client = gspread.authorize(creds)
 SheetID = os.getenv("Sheet_ID")
 sheet = client.open_by_url(f'https://docs.google.com/spreadsheets/d/{SheetID}/edit#gid=478985565')
-sheets = sheet.worksheets()[11:]
+sheets = [i.title for i in sheet.worksheets()[12:]]
 
 
 dataBase = sqlite3.connect('nba_ids.db')
 base = dataBase.cursor()
 base.execute('''CREATE TABLE IF NOT EXISTS homeAway
-             (teamName TEXT, 2p%Home TEXT, 2p%Away TEXT, 3p%Home TEXT
-             3p%Away TEXT, 2p%Against)''')
+             (playerName TEXT, twoPointHome TEXT, twoPointAway TEXT, threePointHome TEXT, 
+             threePointAway TEXT, FTHome TEXT, FTAway TEXT, gamesPlayedHome TEXT, gamesPlayedAway TEXT)''')
 dataBase.commit()
+
+for sheetName in sheets[:2]:
+    sh = sheet.worksheet(sheetName)
+    df = pd.DataFrame(sh.get("I3:AE120"))
+    df.columns = ['Location', 'Opp', 'Result', 'GS', 'MP', 'FG', 'FGA', 'FG%', 
+                    '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 
+                    'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
+    print(df)
+    
+    
 
