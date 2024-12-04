4194dc9ef430991580bd09e181878cdf600a3957
Gavin Payne <g.payne2189@gmail.com>
2024-12-03 23:32:16 -0700

    Some improvements, 97 % done

--- EuphoricReg.py
+++ EuphoricReg.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
@@ -18,6 +20,7 @@ import numpy as np
 import re
 import unicodedata
 import time
+
 
 load_dotenv()
 googleJSON = os.getenv('GOOGLE_CLOUD_SERVICE_ACCOUNT')
@@ -35,12 +38,14 @@ def regularize_name(name):
     name = re.sub(r"[’'`]", "", name)
     name = re.sub(r"[-]", " ", name)
     name = re.sub(r"[^a-zA-Z\s]", "", name)
-    name = name.split()
-    name = "_".join(name)
+    name_parts = name.split()
+    suffixes = {"jr", "sr", "ii", "iii", "iv", "v"}
+    if name_parts and name_parts[-1].lower() in suffixes:
+        name_parts.pop()
+    name_parts = [part.capitalize() for part in name_parts]
+    name = "_".join(name_parts)
     
     return name
-
-#Open link and wait for it to load
 driver = webdriver.Chrome()
 url = "https://www.pbpstats.com/possession-finder/nba"
 driver.get(url)
@@ -55,10 +60,7 @@ driver.execute_script("arguments[0].click();", button)
 
 
 
-for i in range(2, 30):
-    nPlayers = wait.until(EC.presence_of_element_located((By.XPATH, '(//div[@class="multiselect__content-wrapper" and contains(., "1 of")])')))
-    nPlayers = nPlayers.find_elements(By.TAG_NAME, 'span')
-    driver.execute_script("arguments[0].click();", nPlayers[2])
+for i in range(21, 30):
     if i > 0:
         teams = wait.until(EC.element_to_be_clickable((By.XPATH, '(//div[@class="multiselect"])[3]')))
         driver.execute_script("arguments[0].click();", teams)
@@ -66,36 +68,37 @@ for i in range(2, 30):
 
         teams = wait.until(EC.presence_of_all_elements_located((By.XPATH, '(//div[@class="multiselect"])[3]//ul[@class="multiselect__content"]//li[@class="multiselect__element"]//span[@class="multiselect__option"]//span')))
         team = teams[i]
-
         driver.execute_script("arguments[0].scrollIntoView();", team)
         driver.execute_script("arguments[0].click();", team)
         time.sleep(2)
-    dropdowns = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "multiselect__content-wrapper")))
+    nPlayers = wait.until(EC.presence_of_element_located((By.XPATH, '(//div[@class="multiselect__content-wrapper" and contains(., "1 of")])')))
+    nPlayers = nPlayers.find_elements(By.TAG_NAME, 'span')
+    driver.execute_script("arguments[0].click();", nPlayers[2])
+    dropdowns = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "multiselect__content-wrapper"))
+)
+    dd = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "multiselect__select")))
+    dd[9].click() 
     playersDropdown = dropdowns[9]
-    players = playersDropdown.find_elements(By.CSS_SELECTOR, "li.multiselect__element span.multiselect__option")
-    for player in players:
-        driver.execute_script("arguments[0].scrollIntoView(true);", player)
-        name = player.find_element(By.TAG_NAME, "span")
-        print(name.text.strip())
-    for i in range(len(players)):
+    
+    players = playersDropdown.find_elements(By.TAG_NAME, "li")[:-2]
+    for i in range(len(players)-1, -1, -1):
         try:
-            players = playersDropdown.find_elements(By.CSS_SELECTOR, "li.multiselect__element span.multiselect__option")
-            driver.execute_script("arguments[0].click();", players[i])
+            name = players[i].get_attribute("textContent").strip()
+            players[i].click()
             pos = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Get Possessions"]')))
             driver.execute_script("arguments[0].click();", pos)
-            remove_buttons = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "custom__remove")))
-            if len(remove_buttons) > 2:
-                remove_buttons[1].click()
-            remove_buttons[1].click()
+            rbs = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "custom__remove")))
+            if len(rbs) > 2:
+                rbs[1].click()
+            rbs[1].click()
             try: 
-                tables = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "vgt-wrap")))
-
+                tables = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "vgt-wrap")))
                 if len(tables) > 1: 
                     table = tables[1]
-
-                    # Extract headers
                     headers = table.find_elements(By.CSS_SELECTOR, "thead th")
-                    columns = [header.text.strip() for header in headers if header.text.strip()]  # Only non-empty headers
+                    columns = [header.text.strip() for header in headers if header.text.strip()]
                     rows = table.find_elements(By.CSS_SELECTOR, "tbody tr")
                     data = []
                     for row in rows:
@@ -103,10 +106,10 @@ for i in range(2, 30):
                         data.append([cell.text.strip() for cell in cells])
                         
                     df = pd.DataFrame(data, columns=columns)
-                    
-                    name = players[i].find_element(By.TAG_NAME, "span")
-                    name = regularize_name(name.text)
-                    base.execute(f'''CREATE TABLE IF NOT EXISTS {name}OnOff
+                    df['Name\nSort table by Name in ascending order'] = df['Name\nSort table by Name in ascending order'].apply(regularize_name)
+                    name = regularize_name(name)
+                    base.execute(f"DROP TABLE IF EXISTS {name}On")
+                    base.execute(f'''CREATE TABLE IF NOT EXISTS {name}On
                                 (Player TEXT, Name TEXT, possessions INTEGER, FG2M INTEGER, FG2A INTEGER, TFGp REAL,
                                 FG3M INTEGER, FG3A INTEGER, ThFGp REAL, idcThFGp REAL, FTM INTEGER, AST2PTS INTEGER,
                                 uAST2PTS INTEGER, AST3PTS INTEGER, uAST3PTS INTEGER, TPAp REAL, idcTPAp REAL,
@@ -115,7 +118,7 @@ for i in range(2, 30):
                     dataBase.commit()
                     for _, row in df.iterrows():
                         base.execute(f'''
-                            INSERT OR REPLACE INTO {name}OnOff (
+                            INSERT OR REPLACE INTO {name}On (
                                 Player, Name, possessions, FG2M, FG2A, TFGp, FG3M, FG3A, ThFGp, idcThFGp, FTM, AST2PTS,
                                 uAST2PTS, AST3PTS, uAST3PTS, TPAp, idcTPAp, ThPAp, thPAr, Qual, eFG, TS, PUT, FG2AB,
                                 FG2ABp, FG3AB, FG3ABp, Usage
@@ -125,7 +128,7 @@ for i in range(2, 30):
                     dataBase.commit()
                     print(name)
             except TimeoutException:
-                print(f"No data for {players[i].text}")
+                print(f"No data for {players[i].get_attribute('textContent')}")
                 
         except StaleElementReferenceException:
             print(f"Stale element encountered for player at index {i}. Retrying...")
